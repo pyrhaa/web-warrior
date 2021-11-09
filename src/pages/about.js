b7dd65ca228c3597d80058b49b1fa3d71c049326
pyrhaa <sboubtane@protonmail.com>
2021-11-09 13:08:08 +0100

    create some pages for test

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+        reprehenderit quis accusamus provident nulla odit ullam molestiae
+        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+        culpa blanditiis asperiores praesentium?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+        reprehenderit quis accusamus provident nulla odit ullam molestiae
+        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+        culpa blanditiis asperiores praesentium?
+      </p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+        reprehenderit quis accusamus provident nulla odit ullam molestiae
+        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+        culpa blanditiis asperiores praesentium?
+      </p>
+    </div>
+  )
+}

d124219084aaf9d9e5ccf3508f401f98c570bd9c
pyrhaa <sboubtane@protonmail.com>
2021-11-11 16:31:43 +0100

    create & import in layout css styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,30 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
-        reprehenderit quis accusamus provident nulla odit ullam molestiae
-        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
-        culpa blanditiis asperiores praesentium?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
-        reprehenderit quis accusamus provident nulla odit ullam molestiae
-        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
-        culpa blanditiis asperiores praesentium?
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
-        reprehenderit quis accusamus provident nulla odit ullam molestiae
-        sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
-        culpa blanditiis asperiores praesentium?
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+          reprehenderit quis accusamus provident nulla odit ullam molestiae
+          sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+          culpa blanditiis asperiores praesentium?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+          reprehenderit quis accusamus provident nulla odit ullam molestiae
+          sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+          culpa blanditiis asperiores praesentium?
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+          reprehenderit quis accusamus provident nulla odit ullam molestiae
+          sapiente dolore nesciunt labore, esse assumenda. Facilis libero qui
+          culpa blanditiis asperiores praesentium?
+        </p>
+      </div>
+    </Layout>
   )
 }
